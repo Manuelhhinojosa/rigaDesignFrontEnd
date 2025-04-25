aae08383cd08e844a48d6e862e3ce48b65c9b656
Manuelhhinojosa <mangud.hinojosa@gmail.com>
2025-04-25 10:52:04 -0400

    updated shopmenu component image as requested

--- src/redux/slices/staticState/imagesSlice.js
+++ src/redux/slices/staticState/imagesSlice.js
@@ -6,7 +6,7 @@ export const imagesSlice = createSlice({
     homeImageUrl:
       "https://res.cloudinary.com/mangud/image/upload/v1745591848/mariselaShopClient/Copia_de_Riga_portada_peq_e9bbiw_l6toni.jpg",
     mainShopMenuImageImageUrl:
-      "https://res.cloudinary.com/mangud/image/upload/v1745591848/mariselaShopClient/Copia_de_Riga_portada_peq_e9bbiw_l6toni.jpg",
+      "https://res.cloudinary.com/mangud/image/upload/v1745592642/mariselaShopClient/Screen_Shot_2025-04-25_at_10.50.29_AM_y9uea5.png",
     mainShopMenuImageImageUrl2:
       "https://res.cloudinary.com/mangud/image/upload/v1745591755/mariselaShopClient/Copia_de_Riga-07_e4akdw_wfyjqa.jpg",
     aboutImageUrl:
